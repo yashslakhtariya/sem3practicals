21e3e8572facf6e17a7a938e25f32c963dc0ae89
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2022-09-01 12:38:28 +0530

    modified:   DBMS/prac_5.sql

--- DBMS/prac_5.sql
+++ DBMS/prac_5.sql
@@ -1,6 +1,6 @@
-CREATE TABLE Student(Roll VARCHAR(11) check(Roll like '211621%') primary key, Name VARCHAR(50), Sem decimal(1) check(Sem between 1 and 8), Branch VARCHAR(3) check(Branch in ('CBA','BDA','CS')), Email varchar(50) unique check(Email like '%@gnu.ac.in'), Contact unique decimal(10));
+CREATE TABLE Student (Roll VARCHAR(11) primary key check(Roll like "211621%"), Name VARCHAR(50), Sem decimal(1) check(Sem between 1 and 8), Branch VARCHAR(3) check(Branch in ('CBA','BDA','CS')), Email varchar(50) UNIQUE check(Email like '%@gnu.ac.in'), Contact decimal(10) unique);
 
-CREATE TABLE Faculty(FID DECIMAL(2) primary key, Name VARCHAR(50), Email VARCHAR(50) unique check(Email like '%@ganpatuniversity.ac.in'), Contact decimal(10));
+CREATE TABLE Faculty(FID DECIMAL(2) primary key, F_Name VARCHAR(50), Email VARCHAR(50) unique check(Email like '%@ganpatuniversity.ac.in'), Contact decimal(10) unique);
 
 CREATE TABLE Subject(SID VARCHAR(7) primary key, S_Name VARCHAR(40), Sem decimal(1) check(Sem between 1 and 8), Branch VARCHAR(3) check(Branch in ('CBA','BDA','CS')));
 
