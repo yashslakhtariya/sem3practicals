629d1344ba5ed5d46caba8dffd572f22ddfff754
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2022-09-09 10:14:03 +0530

    new file:   yash_lakhtariya/bin/yash_lakhtariya/constr.class 	new file:   yash_lakhtariya/src/yash_lakhtariya/constr.java

---
+++ yash_lakhtariya/src/yash_lakhtariya/constr.java
@@ -0,0 +1,25 @@
+package yash_lakhtariya;
+
+class ysl
+{
+     String h;
+     ysl(String y)
+     {
+          y=h;
+     }
+     void rvrse(String y)
+     {
+          int a=0;
+          while(a != y.length())
+          {
+               for(int i=0; i<y.length(); i++)
+               {
+                    
+               }
+          }
+     }
+}
+
+public class constr {
+     
+}
