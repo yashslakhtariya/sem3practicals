67d9baa2f1a2ba8c261d02d7e2c80933549f7c9e
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2022-09-16 09:53:07 +0530

    haribol

--- DS/Haribol/src/OOP_PR5.java
+++ DS/Haribol/src/OOP_PR5.java
@@ -9,9 +9,27 @@ class sqrert
     }
 }
 
-class ecldn
+class ecldn extends sqrert
 {
+    void calc()
+    {
 
+    }
+}
+
+class pnt extends ecldn
+{
+    double[] x = new double[2];
+    double[] y = new double[2];
+    void inpt()
+    {
+        Scanner ysl = new Scanner(System.in);
+        for (int i=0; i<2; i++)
+        {
+            x[i] = ysl.nextDouble();
+            y[i] = ysl.nextDouble();
+        }
+    }
 }
 public class OOP_PR5
 {
