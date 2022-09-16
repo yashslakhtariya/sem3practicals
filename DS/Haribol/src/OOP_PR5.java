cfeac9aaa42b5c2461b1cb0cd50f0047dd388b16
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2022-09-16 09:38:10 +0530

    haribol

---
+++ DS/Haribol/src/OOP_PR5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+class sqrert
+{
+    double root(double y)
+    {
+        return Math.sqrt(y);
+    }
+}
+
+class ecldn
+{
+
+}
+public class OOP_PR5
+{
+
+}
