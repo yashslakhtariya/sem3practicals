b1b3da727aa3b732c377499e3dad221437719d7b
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2022-10-22 21:08:36 +0530

    haribol

--- OOP/Hare_Krishna/src/Hare_Krishna/Prac_12.java
+++ OOP/Hare_Krishna/src/Hare_Krishna/Prac_12.java
@@ -1,9 +1,45 @@
 package Hare_Krishna;
 
+import java.util.Scanner;
+
+abstract class Book
+{
+    String title;
+
+    abstract void setTitle(String b);
+/*    String getTitle()
+    {
+        return title;
+    }
+*/
+}
+
+interface TitleBook
+{
+    void setTitle(String tb);
+    public String getTitle();
+}
+
+class MyBook extends Book implements TitleBook
+{
+    public void setTitle(String mb)
+    {
+        title = mb;
+    }
+    public String getTitle()
+    {
+        return title;
+    }
+}
 public class Prac_12
 {
     public static void main(String... s)
     {
-
+        Scanner scnr = new Scanner(System.in);
+        System.out.print("\nEnter title of the book : ");
+        String twelve = scnr.nextLine();
+        MyBook mb1 = new MyBook();
+        mb1.setTitle(twelve);
+        System.out.print("\nThe title of the book is : " + mb1.getTitle() + "\n");
     }
 }
