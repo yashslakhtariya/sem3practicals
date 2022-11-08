4f841452ff1eb7f90867530da5a31afb8512147e
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2022-11-09 05:22:44 +0530

    haribol

---
+++ OOP/Hare_Krishna/src/Hare_Krishna/Prac_14.java
@@ -0,0 +1,33 @@
+package Hare_Krishna;
+
+import java.util.Scanner;
+
+class Underage extends Exception
+{
+    Underage(String err)
+    {
+        System.out.println(err);
+    }
+}
+
+public class Prac_14
+{
+    public static void main(String... s) throws Underage
+    {
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("Enter you details for check voting eligibility : ");
+        System.out.print("\n\tName : ");
+        String name = scnr.nextLine();
+        System.out.print("\n\tAge : ");
+        int age = scnr.nextInt();
+        System.out.println();
+        if(age>18)
+        {
+            System.out.println("You are eligible to vote in India!");
+        }
+        else
+        {
+            throw new Underage("Oops! You are not eligible to vote!");
+        }
+    }
+}
