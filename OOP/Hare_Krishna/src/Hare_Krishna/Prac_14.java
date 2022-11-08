069a283ce771c0d294e5d44f57f9b1ee531d171a
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2022-11-09 05:23:50 +0530

    haribol

--- OOP/Hare_Krishna/src/Hare_Krishna/Prac_14.java
+++ OOP/Hare_Krishna/src/Hare_Krishna/Prac_14.java
@@ -1,5 +1,4 @@
 package Hare_Krishna;
-
 import java.util.Scanner;
 
 class Underage extends Exception
@@ -15,7 +14,7 @@ public class Prac_14
     public static void main(String... s) throws Underage
     {
         Scanner scnr = new Scanner(System.in);
-        System.out.println("Enter you details for check voting eligibility : ");
+        System.out.println("\nEnter you details for check voting eligibility : ");
         System.out.print("\n\tName : ");
         String name = scnr.nextLine();
         System.out.print("\n\tAge : ");
