7fea0485be71c15272a8db0870ffe116712d9f39
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2022-11-11 12:59:21 +0530

    haribol

--- OOP/Hare_Krishna/src/Hare_Krishna/Prac_15.java
+++ OOP/Hare_Krishna/src/Hare_Krishna/Prac_15.java
@@ -1,2 +1,36 @@
-package Hare_Krishna;public class Prac_15 {
+package Hare_Krishna;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+class yslException extends IOException
+{
+    yslException()
+    {
+        super();
+    }
+    int flt2int(float y)
+    {
+        return (int) y;
+    }
+}
+
+public class Prac_15
+{
+    static public void main(String... s)
+    {
+        int pr;
+        float roi;
+        float yrs;
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("\nEnter the details for calculating amount : ");
+
+        System.out.print("\n\tROI : ");
+        roi = scnr.nextFloat();
+        System.out.print("\n\tPrincipal : ");
+        pr = scnr.nextInt();
+        float temp = (float) pr;
+
+        System.out.print("\n\tNumber of years : ");
+    }
 }
--- OOP/Hare_Krishna/src/Hare_Krishna/demo.java
+++ OOP/Hare_Krishna/src/Hare_Krishna/demo.java
@@ -1,5 +1,12 @@
 package Hare_Krishna;
 
-public class demo {
-     
+public class demo
+{
+    public static void main(String[] args) {
+        float t = 2.143f;
+        int yrs = 26/12;
+        int hold = 26-(yrs*12);
+        System.out.println(yrs + " " + hold);
+
+    }
 }
