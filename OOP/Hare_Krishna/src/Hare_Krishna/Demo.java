77e4d35cdc46d0da3c6755cc3fd4c799714aae0b
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2022-11-18 08:21:08 +0530

    haribol

--- OOP/Hare_Krishna/src/Hare_Krishna/Demo.java
+++ OOP/Hare_Krishna/src/Hare_Krishna/Demo.java
@@ -1,9 +1,33 @@
 package Hare_Krishna;
+import org.mariadb.jdbc.Driver;
+
+import java.sql.*;
 
 public class Demo
 {
-    public static void main(String[] args)
+    public static void main(String... s)
     {
+        try
+        {
+            String name = "Yash";
+            int ID = 64;
+            String query = "insert into demo values(" + ID + ", " + "\"" + name + "\"" +")";
+            Connection con = DriverManager.getConnection("jdbc:mariadb://localhost:3306/demo","yash","krsna");
+            Statement st = con.createStatement();
+            int result = st.executeUpdate(query);
+            System.out.println(result + " records affected");
+            ResultSet rs = st.executeQuery("select * from demo");
+            while (rs.next())
+            {
+                System.out.println(rs.getInt(1) + " " + rs.getString(2) + " ");
+            }
+            con.close();
+
+        }
+        catch (Exception exc)
+        {
+            exc.printStackTrace();
+        }
         System.out.println("Demo program");
     }
 }
