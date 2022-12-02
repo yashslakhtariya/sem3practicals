e317bcff1f487218f584f232e95205365211efd0
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2022-12-02 10:41:31 +0530

    haribol

--- OOP/Hare_Krishna/src/Hare_Krishna/Prac_20.java
+++ OOP/Hare_Krishna/src/Hare_Krishna/Prac_20.java
@@ -1,2 +1,22 @@
-package Hare_Krishna;public class Prac_20 {
+package Hare_Krishna;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Prac_20
+{
+    static public void main(String... s)
+    {
+        Scanner sc = new Scanner(System.in);
+        Queue<String> q = new LinkedList<>();
+        for (int i = 0; i < 5; i++) {
+            q.add(sc.nextLine());
+        }
+        System.out.println(q);
+        q.remove();
+        q.remove();
+        q.add("Semester 6: Pascal");
+        System.out.println(q);
+    }
 }
